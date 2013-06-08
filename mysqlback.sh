d15205cff54033d8fc972c79adc21f4c8c05cd4f
huangyingw@linux_programming <huangyingw@gmail.com>
2013-06-07 17:42:34 -0700

    mysqlback.sh works now

--- mysqlback.sh
+++ mysqlback.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
+TARGET="$1"
 DB=`cat /opt/xetusbase/tomcat/conf/catalina.properties |grep WEBAPP_DBNAME|awk '{print $3}'`
-echo $DB
-#mysql -uroot -pbl0ss0m -h127.0.0.1 xetusloan_folder_hta
-
+mysqldump -uroot -pbl0ss0m -h127.0.0.1 $DB > "$TARGET"
