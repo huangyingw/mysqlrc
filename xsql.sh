7f91c93544bdd161ec1d780af6dc42109012f1bb
huangyingw@linux_programming <huangyingw@gmail.com>
2013-06-12 10:55:54 -0700

    add db parameter in xsql.sh

--- xsql.sh
+++ xsql.sh
@@ -2,12 +2,13 @@
 if [ -z "$1" ];
 then
   echo Please provide the specified sql file
-else
-  user="root"
-  if [ -f $HOME/sqlrc/passwd ]; then
-    PASSWD=`cat $HOME/sqlrc/passwd |grep xspasswd|awk '{print $3}'`
-  else
-    PASSWD="1qaz@WSX"
-  fi
-  mysql -u"$user" -p"$PASSWD" -h127.0.0.1< "$1"
+  exit 1
 fi
+user="root"
+if [ -f $HOME/sqlrc/passwd ]; then
+  PASSWD=`cat $HOME/sqlrc/passwd |grep xspasswd|awk '{print $3}'`
+else
+  PASSWD="1qaz@WSX"
+fi
+DB=`cat /opt/xetusbase/tomcat/conf/catalina.properties |grep WEBAPP_DBNAME|awk '{print $3}'`
+mysql -u"$user" -p"$PASSWD" -h127.0.0.1 $DB< "$1"
