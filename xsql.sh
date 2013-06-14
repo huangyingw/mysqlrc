bc55138ff68b86fbbafe61175ced3c600b05cc59
huangyingw@linux_programming <huangyingw@gmail.com>
2013-06-14 08:55:17 -0700

    code refine on xsql.sh

--- xsql.sh
+++ xsql.sh
@@ -4,6 +4,7 @@ then
   echo Please provide the specified sql file
   exit 1
 fi
+TARGET="$1"
 user="root"
 if [ -f $HOME/sqlrc/passwd ]; then
   PASSWD=`cat $HOME/sqlrc/passwd |grep xspasswd|awk '{print $3}'`
@@ -11,4 +12,4 @@ else
   PASSWD="1qaz@WSX"
 fi
 DB=`cat /opt/xetusbase/tomcat/conf/catalina.properties |grep WEBAPP_DBNAME|awk '{print $3}'`
-mysql -u"$user" -p"$PASSWD" -h127.0.0.1 $DB< "$1"
+mysql -u"$user" -p"$PASSWD" -h127.0.0.1 $DB < "$TARGET"
