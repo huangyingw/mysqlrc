1f38b11e95f54a91cab65afbb3dfa2f535194670
huangyingw@linux_programming <huangyingw@gmail.com>
2013-09-11 16:00:08 -0700

    update the passwd location

--- mysqlback.sh
+++ mysqlback.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 TARGET="$1"
-PASSWD=`cat $HOME/sqlrc/passwd |grep xspasswd|awk '{print $3}'`
+PASSWD=`cat $HOME/.passwd |grep xspasswd|awk '{print $3}'`
 DB=`cat /opt/xetusbase/tomcat/conf/catalina.properties |grep WEBAPP_DBNAME|awk '{print $3}'`
 mysqldump -uroot -p"$PASSWD" -h127.0.0.1 $DB > "$TARGET"
