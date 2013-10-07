4fded832b6cfed8d1b3fd696d24a2081ae261ed3
huangyingw@linux_programming <huangyingw@gmail.com>
2013-10-07 15:34:23 -0700

    update the way I get the mysql DB instance

--- mysqlback.sh
+++ mysqlback.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 TARGET="$1"
 PASSWD=`cat $HOME/.passwd |grep xspasswd|awk '{print $3}'`
-DB=`cat /opt/xetusbase/tomcat/conf/catalina.properties |grep WEBAPP_DBNAME|awk '{print $3}'`
-mysqldump -uroot -p"$PASSWD" -h127.0.0.1 $DB > "$TARGET"
+DB=`cat config/xetusone.properties |awk -F'=' '/database.primary.name/{print $2}'`
+if [ ! -z "$DB" ];
+then
+    mysqldump -uroot -p"$PASSWD" -h127.0.0.1 $DB > "$TARGET"
+fi
--- xsql.sh
+++ xsql.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 if [ -z "$1" ];
 then
-  echo Please provide the specified sql file
-  exit 1
+    echo Please provide the specified sql file
+    exit 1
 fi
 TARGET="$1"
 user="root"
 if [ -f $HOME/.passwd ]; then
-  PASSWD=`cat $HOME/.passwd |grep xspasswd|awk '{print $3}'`
+    PASSWD=`cat $HOME/.passwd |grep xspasswd|awk '{print $3}'`
 else
-  PASSWD="1qaz@WSX"
+    PASSWD="1qaz@WSX"
 fi
-DB=`cat /opt/xetusbase/tomcat/conf/catalina.properties |grep WEBAPP_DBNAME|awk '{print $3}'`
-mysql -u"$user" -p"$PASSWD" -h127.0.0.1 $DB < "$TARGET"
+DB=`cat config/xetusone.properties |awk -F'=' '/database.primary.name/{print $2}'`
+if [ ! -z "$DB" ];
+then
+    mysql -u"$user" -p"$PASSWD" -h127.0.0.1 $DB < "$TARGET"
+fi
