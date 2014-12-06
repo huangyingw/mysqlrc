4e562a63825e4c49872d7f397ab3c619ee14d791
huangyingw@linux_programming <huangyingw@gmail.com>
2014-12-06 11:48:36 -0800

    n

--- xsql.sh
+++ xsql.sh
@@ -1,18 +1,4 @@
 #!/bin/sh
-if [ -z "$1" ];
-then
-    echo Please provide the specified sql file
-    exit 1
-fi
-TARGET="$1"
-user="root"
-if [ -f $HOME/.passwd ]; then
-    PASSWD=`cat $HOME/.passwd |grep xspasswd|awk '{print $3}'`
-else
-    PASSWD="1qaz@WSX"
-fi
-DB=`cat config/xetusone.properties |awk -F'=' '/database.primary.name/{print $2}'`
-if [ ! -z "$DB" ];
-then
-    mysql -u"$user" -p"$PASSWD" -h127.0.0.1 $DB < "$TARGET"
-fi
+HOST="$1"
+TARGET="$2"
+mysql -ucondor -ptemp123 -h"$HOST" recdeveloper < "$TARGET"
