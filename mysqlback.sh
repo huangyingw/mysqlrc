959934723ef9d8bef1c61ce30f303af9fd459c7b
huangyingw@linux_programming <huangyingw@gmail.com>
2014-12-06 11:48:36 -0800

    before initiating new branch dev from master,n

--- mysqlback.sh
+++ mysqlback.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-TARGET="$1"
-PASSWD=`cat $HOME/.passwd |grep xspasswd|awk '{print $3}'`
-DB=`cat config/xetusone.properties |awk -F'=' '/database.primary.name/{print $2}'`
-if [ ! -z "$DB" ];
-then
-    mysqldump -uroot -p"$PASSWD" -h127.0.0.1 $DB > "$TARGET"
-fi
+HOST="$1"
+TARGET="$2"
+mysqldump -ucondor -ptemp123 -h"$HOST" recdeveloper > "$TARGET"
