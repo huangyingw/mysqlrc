612f04277f15ba934c22b58d803e36d360d61484
huangyingw@linux_programming <huangyingw@gmail.com>
2015-01-07 13:08:08 -0800

    n

--- xsql.sh
+++ xsql.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-HOST="$1"
-TARGET="$2"
-mysql -ucondor -ptemp123 -h"$HOST" recdeveloper < "$TARGET"
+file=${1}
+user=`cat ~/sqlrc/.login |awk -F'=' '/user/{print $2}'`
+password=`cat ~/sqlrc/.login |awk -F'=' '/password/{print $2}'`
+host=`cat ~/sqlrc/.login |awk -F'=' '/host/{print $2}'`
+dbinstance=`cat ~/sqlrc/.login |awk -F'=' '/dbinstance/{print $2}'`
+mysql -u"$user" -p"$password" -h${host} ${dbinstance} < ${file}
