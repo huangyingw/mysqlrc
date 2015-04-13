7e1cffd806d0d5593e74b0880548bea50c89295a
huangyingw@linux_programming <huangyingw@gmail.com>
2015-04-13 15:39:42 -0700

    n

--- xsql.sh
+++ xsql.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 file=${1}
-user=`cat ~/sqlrc/.login |awk -F'=' '/user/{print $2}'`
-password=`cat ~/sqlrc/.login |awk -F'=' '/password/{print $2}'`
-host=`cat ~/sqlrc/.login |awk -F'=' '/host/{print $2}'`
-dbinstance=`cat ~/sqlrc/.login |awk -F'=' '/dbinstance/{print $2}'`
+user=`cat ~/loadrc/sqlrc/.login |awk -F'=' '/user/{print $2}'`
+password=`cat ~/loadrc/sqlrc/.login |awk -F'=' '/password/{print $2}'`
+host=`cat ~/loadrc/sqlrc/.login |awk -F'=' '/host/{print $2}'`
+dbinstance=`cat ~/loadrc/sqlrc/.login |awk -F'=' '/dbinstance/{print $2}'`
 mysql -u"$user" -p"$password" -h${host} ${dbinstance} < ${file}
